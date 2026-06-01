3feea85a6428c55ea87acdb82f45e5df86ddc15b
AGATE Technical Auditor <catinlahat@gmail.com>
2026-06-01 18:06:58 -0500

    ci(core): provision main.py Kivy entrypoint for remote buildozer compilation

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        layout.add_widget(Label(text='Base Waste Harvester'))
+        return layout
+
+if __name__ == '__main__':
+    MyApp().run()
