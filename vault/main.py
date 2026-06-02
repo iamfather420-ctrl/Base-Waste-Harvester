3fc603b3fe67be22d91509809fa7d4de8d417f1b
iamfather420-ctrl <i.am.father.420@gmail.com>
2026-06-01 19:53:26 -0500

    fix(build): place main.py in vault/ directory per buildozer.spec source.dir configuration

---
+++ vault/main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        layout.add_widget(Label(text='Base Waste Harvester'))
+        return layout
+
+
+if __name__ == '__main__':
+    MyApp().run()
